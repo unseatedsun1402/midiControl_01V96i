56b405ad08b45e269bff4cf4d13fa45b8bf3af8e
Ethan <unseatedsun1402>
2022-10-04 16:50:17 +0100

    building web controller

--- webserver.js
+++ webserver.js
@@ -1,13 +1,36 @@
-import http from "http";
+const http = require('http');
+const fs = require('fs').promises;
+//const { isBuffer } = require('util');
 const host = 'localhost';
 const port = 8000;
+const {spawn} = require('child_process');
+
+const ver = spawn('python',['--version.py']);
+ver.stdout.on('data', (data)=>{
+    console.log('stdout: ${data}')
+});
+ver.stderr.on('data',(data)=>{
+    console.error('stderr: ${data}')
+});
+
+let indexFile;
 
 const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "text/html");
     res.writeHead(200);
-    res.end("My first server!");
+    res.end(indexFile);
 };
 
 const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-});
+
+fs.readFile(__dirname + "/index.html")
+    .then(contents => {
+        indexFile = contents;
+        server.listen(port, host, () => {
+            console.log(`Server is running on http://${host}:${port}`);
+        });
+    })
+    .catch(err => {
+        console.error(`Could not read index.html file: ${err}`);
+        process.exit(1);
+    });
