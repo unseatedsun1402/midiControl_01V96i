30f028cbdb4bb6ba4bc88317cc8e11153a387809
Ethan Blood <ethan.blood@hotmail.co.uk>
2023-03-08 09:31:14 +0000

    changed vu

--- meterView/VU.py
+++ meterView/VU.py
@@ -8,8 +8,8 @@ class Meter():
         """draws a meter for given input"""
         for i in range (0,self.level, 6):
             if i < 20: 
-                pygame.draw.rect(context (0, 192, 0), (self.id*10, (475-i*12), 30, 10))
+                pygame.draw.rect(context (0, 192, 0), (self.id*10, (475-i*6), 10, 5))
             elif i >= 20 and i < 30:
-                pygame.draw.rect(context, (255, 255, 0), (self.id*10, (475-i*12), 30, 10))
+                pygame.draw.rect(context, (255, 255, 0), (self.id*10, (475-i*6), 10, 5))
             else:
-                pygame.draw.rect(context, (255, 0, 0), (self.id*10, (475-i*12), 30, 10))
+                pygame.draw.rect(context, (255, 0, 0), (self.id*10, (475-i*6), 10, 5))
